905c4ef05a20ad11be12eb56ad5f0dc4fe9cae3c
Jeremy Quinto <jpquinto@ucsd.edu>
2024-02-14 11:59:28 -0800

    fixed broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/tutorial/overview">
+            to="/docs/category/welcome-to-nohire">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
